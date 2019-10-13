2cbea1fd0ed40228bac632884d96a21b6feb066e
zingp <605156398@qq.com>
2019-10-13 21:38:16 +0800

    添加训练命令

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
 python3 eval.py /root/liuyouyuan/pyproject/pointer-generator-pytorch/log/train_1569842742/model/model_5000_1569846703
+
+
+nohup python decode.py ./weibo_log/train_1570885369/model/model_415000_20191013_104358 >weibo_no_coverage.log 2>&1 &
