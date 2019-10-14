0d3d206fb8db26327d75a520eba17261895f3dd2
zingp <605156398@qq.com>
2019-10-14 15:28:52 +0800

    修改一些配置

--- config.py
+++ config.py
@@ -1,25 +1,25 @@
 import os
 import torch
-root_dir = "/search/odin/liuyouyuan/pyproject/data/finished_files"
-#root_dir = "/search/odin/liuyouyuan/pyproject/data/weibo/finished_files"
+# root_dir = "/search/odin/liuyouyuan/pyproject/data/finished_files"
+root_dir = "/search/odin/liuyouyuan/pyproject/data/weibo/finished_files"
 train_data_path = os.path.join(root_dir, "chunked/train_*")
 eval_data_path = os.path.join(root_dir, "val.bin")
-decode_data_path = os.path.join(root_dir, "test.bin")
+decode_data_path = os.path.join(root_dir, "val.bin")
 vocab_path = os.path.join(root_dir, "vocab")
 log_root = "./cnn_coverage_log"
-# log_root = "./weibo_log"
+#log_root = "./weibo_log"
 
 # Hyperparameters
 hidden_dim= 256
 emb_dim= 128
 batch_size= 16
-max_enc_steps=400
-#max_enc_steps=200
-max_dec_steps=100
-#max_dec_steps=40
+# max_enc_steps=400
+max_enc_steps=200
+# max_dec_steps=100
+max_dec_steps=40
 beam_size=4
-min_dec_steps=35
-#min_dec_steps=20
+# min_dec_steps=35
+min_dec_steps=20
 vocab_size=50_000
 
 lr=0.15
@@ -30,8 +30,8 @@ trunc_norm_init_std=1e-4
 max_grad_norm=2.0
 
 pointer_gen = True
-# is_coverage = False
 is_coverage = True
+# is_coverage = True
 cov_loss_wt = 1.0
 
 eps = 1e-12
@@ -43,4 +43,4 @@ lr_coverage=0.15
 use_gpu=True
 USE_CUDA = use_gpu and torch.cuda.is_available()     # 是否使用GPU
 NUM_CUDA = torch.cuda.device_count()
-DEVICE = torch.device("cuda:4" if USE_CUDA else 'cpu')
+DEVICE = torch.device("cuda:2" if USE_CUDA else 'cpu')
--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@ python3 eval.py /root/liuyouyuan/pyproject/pointer-generator-pytorch/log/train_1
 
 
 nohup python decode.py ./weibo_log/train_1570885369/model/model_415000_20191013_104358 >weibo_no_coverage.log 2>&1 &
+
+nohup python train.py -m weibo_log/train_1570973725/model/model_495000_20191014_111027 >wb_coverage_log  2>&1 &
