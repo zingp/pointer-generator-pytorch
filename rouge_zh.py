0f2596689fd50d1a53ff77cee25b0c69c752381c
zingp <605156398@qq.com>
2019-10-30 23:39:06 +0800

    计算中文rouge

---
+++ rouge_zh.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import glob
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        cont = f.read()
+    return cont
+
+def gen_sentence(filename):
+    cont = read_file(filename)
+    cont = "".join(cont.split(" "))
+    return cont
+
+def compute_rouge_n(text1, text2, n):
+    def ngram(text, n):
+        leng = len(text)
+        word_dic = {}
+        for i in range(0, leng, n):
+            start = i
+            words = ""
+            if leng - start < n:
+                break
+            else:
+                words = text[start: start+n]
+                word_dic[words] = 1
+        return word_dic
+    dic1 = ngram(text1, n)
+    dic2 = ngram(text2, n)
+    x = 0
+    y = len(dic2)
+    for w in dic1:
+        if w in dic2:
+            x += 1
+    rouge = x / y
+    return rouge if rouge <=1.0 else 1.0
+
+
+def avg_rouge(ref_dir, dec_dir, n):
+    ref_files = os.path.join(ref_dir, "*reference.txt")
+    filelist = glob.glob(ref_files)
+    scores_list = []
+    for ref_file in filelist:
+        basename = os.path.basename(ref_file)
+        number = basename.split("_")[0]
+        dec_file = os.path.join(dec_dir, "{}_decoded.txt".format(number))
+        dec_cont = gen_sentence(dec_file)
+        ref_cont = gen_sentence(ref_file)
+        score = compute_rouge_n(dec_cont, ref_cont, n)
+        scores_list.append(score)
+    return sum(scores_list) / len(scores_list)
+
+
+if __name__ == "__main__":
+    #root_dir = "./logs/weibo_log/decode_model_495000_20191014_111027/"
+    root_dir = "./logs/weibo/decode_model_510000_20191030_014457/"
+    ref_dir = os.path.join(root_dir, "rouge_ref")
+    dec_dir = os.path.join(root_dir, "rouge_dec_dir")
+    for i in range(1,4):
+        print("ROUGE-{} : {:.4}".format(i, avg_rouge(ref_dir, dec_dir, i)))
+
